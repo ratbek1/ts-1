118b2b2aea75d2e33057a20c9dbc62c59e43b756
chegebaevraatbek <tasabekovraatbek999@gmail.com>
2023-05-09 18:31:05 +0600

    mm

---
+++ index.js
@@ -0,0 +1,27 @@
+// let num:number = 5
+// num = 10
+// let str:string = "raatbek"
+// let sun: boolean = false
+// console.log(num)
+//
+// const tas:string = "false"
+//
+// function task(a:number,b:number):string {
+//     return a + b + "" + false + undefined
+// }
+//
+// console.log(task(5,5))
+
+var obj = {
+    id: 15,
+    name: "Raatbek",
+    age: 17,
+    job: "developer",
+    car: "BMW M5",
+    wife: false,
+    music: {
+        eminem: "supermen",
+        list: "5"
+    }
+};
+console.table(obj);
---
+++ index.ts
@@ -0,0 +1,76 @@
+// let num:number = 5
+// num = 10
+// let str:string = "raatbek"
+// let sun: boolean = false
+// console.log(num)
+//
+// const tas:string = "false"
+//
+// function task(a:number,b:number):string {
+//     return a + b + "" + false + undefined
+// }
+//
+// console.log(task(5,5))
+
+// let obj:int = {
+//     id: 15,
+//     name: "Raatbek",
+//     age: 17,
+//     job: "developer",
+//     car: "BMW M5",
+//     wife: false,
+//     music: {
+//         eminem: "supermen",
+//         list: "5"
+//     }
+// }
+
+// interface int {
+//     id: number
+//     name: string
+//     age: number
+//     job: string
+//     car: string
+//     wife: boolean
+//     music: {
+//        [key: string]: string
+//     }
+// }
+//
+// console.table(obj)
+
+
+// generics <>
+// type TypeUser<T> = T
+// let str: TypeUser<string> = "Raatbek"
+// let num: TypeUser<number> = 17
+
+// type TypeUser = {
+//     id: number
+//     name: string
+//     age: number
+//     job: string
+//     car: string
+//     wife: boolean
+// }
+//
+// let obj: TypeUser = {
+//     id: 15,
+//     name: "Raatbek",
+//     age: 17,
+//     job: "developer",
+//     car: "BMW M5",
+//     wife: false,
+// }
+
+// function task(a: number,b: number): number {
+//     return a * b
+// }
+// task(4,5)
+
+
+// void - return жазылбаганда колдонудат
+// function task(a: number,b: number): void  {
+//     console.log(a * b)
+// }
+// task(4,5)
